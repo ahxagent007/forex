a85244537217f731f47f397a3cff0f3d1b2a2502
AHXagent007 <ahx.agent007@gmail.com>
2024-12-11 18:37:11 +0600

    latest

--- strategies/mt5_utils.py
+++ strategies/mt5_utils.py
@@ -38,10 +38,10 @@ def initialize_mt5():
     password = 'ABCabc123!@#'
     server = 'Exness-MT5Trial6'
 
-    # ## PRO NEW
-    # login = 182331894
-    # password = 'ABCabc123!@#'
-    # server = 'Exness-MT5Trial6'
+    ## PRO NEW
+    login = 182331894
+    password = 'ABCabc123!@#'
+    server = 'Exness-MT5Trial6'
 
     timeout = 10000
     portable = False
