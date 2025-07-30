61902cbbf64b9b13f382c83e9bfa1c0d70330c2a
AWS Xian <xin.xian.xian@gmail.com>
2025-07-30 22:23:16 +0000

    server

--- strategies/mt5_utils.py
+++ strategies/mt5_utils.py
@@ -282,23 +282,6 @@ def trade_order_wo_sl(symbol, tp_point, lot, action, magic=False):
 
     print(symbol, 'Spread pip: ', spread)
 
-    spread_dict = {
-        'EURUSD': 15,
-        'EURJPY': 15,
-        'USDJPY': 15,
-        'XAUUSD': 150,
-        'BTCUSD': 2300,
-        'GBPUSD': 15,
-        'AUDUSD': 20,
-        'NZDUSD': 20,
-        'USDCHF': 20,
-        'EURGBP': 20,
-        'USDCAD': 20,
-    }
-
-    if spread > spread_dict[symbol]:
-        print('High Spread')
-        return None
 
     deviation = 20
     MAGIC_NUMBER = get_magic_number()
@@ -1218,3 +1201,61 @@ def cancel_all_pending_orders():
                 print(f"✅ Order {order.ticket} canceled successfully")
             else:
                 print(f"⚠️ Could not cancel order {order.ticket}. Retcode: {result.retcode}")
+                
+def calculate_lot_size(symbol, sl_diff):
+
+    risk = 2
+
+    balance = get_balance()
+
+    pip_multiplier = {
+        'GBPUSD': 10000,
+        'USDCHF': 10000,
+        'USDJPY': 100,
+        'US30': 1,
+        'EURGBP': 10000,
+        'AUDUSD': 10000,
+        'XAUUSD': 100,
+        'EURUSD': 10000,
+        'BTCUSD': 1
+    }
+
+    sl_pip = round(sl_diff * pip_multiplier[symbol])
+
+    pip_value = {
+        'GBPUSD': 10,
+        'USDCHF': 10.97,
+        'USDJPY': 6.48,
+        'US30': 1,
+        'EURGBP': 12.48,
+        'AUDUSD': 10,
+        'XAUUSD': 1,
+        'EURUSD': 10,
+        'BTCUSD': 1
+    }
+
+    lot_size = round(balance * (risk / 100) / (pip_value[symbol] * sl_pip), 2)
+
+    return lot_size
+
+def calculate_lot_size_point(symbol, sl_point):
+
+    risk = 2
+
+    balance = get_balance()
+
+    pip_value = {
+        'GBPUSD': 10,
+        'USDCHF': 10.97,
+        'USDJPY': 6.48,
+        'US30': 1,
+        'EURGBP': 12.48,
+        'AUDUSD': 10,
+        'XAUUSD': 1,
+        'EURUSD': 10,
+        'BTCUSD': 0.1
+    }
+    # Lot Size = (Account Balance × Risk %) / (Stop Loss (in pips) × Pip Value per lot)
+    lot_size = round(balance * (risk / 100) / (pip_value[symbol] * sl_point), 2)
+
+    return round(lot_size * 10, 2)
---
+++ strategies/price_distance.py
@@ -0,0 +1,79 @@
+import time
+
+from mt5_utils import get_live_data, initialize_mt5, trade_order_price, calculate_lot_size, get_all_positions
+
+mt5 = initialize_mt5()
+
+SYMBOL_LIST = ['GBPUSD', 'USDCHF', 'USDJPY', 'US30', 'EURGBP', 'AUDUSD', 'XAUUSD', 'EURUSD']
+
+def get_trade_max_percent(symbol):
+    percent_dit = {
+        'GBPUSD': 0.5,
+        'USDCHF': 0.6,
+        'USDJPY': 0.6,
+        'US30': 0.5,
+        'EURGBP': 0.7,
+        'AUDUSD': 0.5,
+        'XAUUSD': 1,
+        'EURUSD': 0.7,
+        'BTCUSD': 1
+    }
+
+    return percent_dit[symbol]
+
+while True:
+    for symbol in SYMBOL_LIST:
+
+
+
+        df = get_live_data(symbol=symbol, time_frame='M5', prev_n_candles=300)
+        df['EMA'] = df['close'].ewm(span=200).mean()
+        current_price = df.iloc[-1].close
+        ema = df.iloc[-1].EMA
+
+        ema_distance_percent = round(((ema - current_price) / current_price) * 100, 2)
+
+        print(f"{symbol} is {ema_distance_percent}% far from the price {current_price}")
+
+        ## CHECK IF RUNNING TRADE
+        running_positions = get_all_positions(symbol)
+
+
+
+        if len(running_positions) > 0:
+            time.sleep(1)
+            continue
+
+        trade_max_percent = get_trade_max_percent(symbol)
+
+        if abs(ema_distance_percent) < trade_max_percent:
+            time.sleep(1)
+            continue
+
+        ema_distance_price = ema - current_price
+        tp_distance = abs(ema_distance_price) / 4
+        sl_distance = tp_distance / 3
+
+        lot = calculate_lot_size(symbol=symbol, sl_diff=sl_distance)
+        print('lot', lot)
+
+        if ema_distance_price > 0:
+            action  = 'buy'
+        else:
+            action = 'sell'
+
+        if action == 'buy':
+            tp_price = current_price + tp_distance
+            sl_price = current_price - sl_distance
+        elif action == 'sell':
+            tp_price = current_price - tp_distance
+            sl_price = current_price + sl_distance
+
+
+        # check if there is any running trade
+        if action:
+            trade_order_price(symbol=symbol, tp_price=tp_price, sl_price=sl_price, lot=lot, action=action, magic=False, code=0, MAGIC_NUMBER=0)
+            #print('trade ', symbol, action)
+
+        time.sleep(1)
+
